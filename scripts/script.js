7f5505ec99d5cfa2323550bffdcbd959a2ec9c12
RachelJB33 <rachel.j.barton33@gmail.com>
2025-03-06 23:15:22 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Navbar Movement
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+  document.querySelector(".sidebar").classList.toggle("change");
+});
+// End of Navbar Movement
