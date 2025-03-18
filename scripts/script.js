521c51b58b0a667e945d02df892193ed9276a2c3
Rachel Barton <rachel.j.barton33@gmail.com>
2025-03-18 00:36:04 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".sidebar").classList.toggle("change");
 });
 // End of Navbar Movement
+
+
+// Team Member "About Me" Movement
+document.querySelectorAll('.story-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+      btn.classList.toggle('change')
+      btn.nextElementSibling.classList.toggle('change')
+  })
+})
+// End of Team Member "About Me" Movement
